7f1cd64a00c9084d5476f4d013c4e09d9436f34b
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-10 19:01:24 +0300

    Задание23

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
 	<div>
     <h1>
-	    <p v-if="isAdmin">+++</p>
-      <p v-else>---</p>
+	    <p v-if="day === 1">Понедельник</p>
+      <p v-if="day === 2">Вторник</p>
+      <p v-if="day === 3">Среда</p>
+      <p v-if="day === 4">Четверг</p>
+      <p v-if="day === 5">Пятница</p>
+      <p v-if="day === 6">Суббота</p>
+      <p v-if="day === 7">Воскресенье</p>
+      <p v-else>Число должно быть от 1 до 7</p>
     </h1>
 	</div>
 </template>
@@ -11,7 +17,7 @@
   export default{
     data(){
       return{
-        isAdmin: false,
+        day: 1,
       }
     },
     methods: {
