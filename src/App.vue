17d81da5b109cdfb97d337fac892d56067483649
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-10 19:08:54 +0300

    Задание24

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <template>
 	<div>
     <h1>
-	    <p v-if="day === 1">Понедельник</p>
-      <p v-if="day === 2">Вторник</p>
-      <p v-if="day === 3">Среда</p>
-      <p v-if="day === 4">Четверг</p>
-      <p v-if="day === 5">Пятница</p>
-      <p v-if="day === 6">Суббота</p>
-      <p v-if="day === 7">Воскресенье</p>
-      <p v-else>Число должно быть от 1 до 7</p>
+	    <p v-if = "age < 18">Подросток</p>
+      <p v-else-if = "age >= 18 && age <= 25">Молодой человек</p>
+      <p v-else-if = "age >= 26">Мужчина</p>
+      <p v-else>Error</p>
     </h1>
 	</div>
 </template>
@@ -17,7 +13,7 @@
   export default{
     data(){
       return{
-        day: 1,
+        age: 21,
       }
     },
     methods: {
