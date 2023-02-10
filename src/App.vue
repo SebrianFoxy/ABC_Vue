5782742fbc6e3129c05dfc30fc56ed4a0bc690a5
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-10 18:56:30 +0300

    Задание22

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
 	<div>
     <h1>
-	    <p v-if="hidden1">Первый абзац</p>
-      <p v-if="hidden2">Второй абзац</p>
-      <p v-if="hidden3">Третий абзац</p>
-      <button @click="reversing1">{{hidden1 ? 'hide': 'show'}}</button>
-      <button @click="reversing2">{{hidden2 ? 'hide': 'show'}}</button>
-      <button @click="reversing3">{{hidden3 ? 'hide': 'show'}}</button>
+	    <p v-if="isAdmin">+++</p>
+      <p v-else>---</p>
     </h1>
 	</div>
 </template>
@@ -15,21 +11,11 @@
   export default{
     data(){
       return{
-        hidden1: true,
-        hidden2: true,
-        hidden3: true,
+        isAdmin: false,
       }
     },
     methods: {
-      reversing1: function(){
-        this.hidden1 = !this.hidden1;
-      },
-      reversing2: function(){
-        this.hidden2 = !this.hidden2;
-      },
-      reversing3: function(){
-        this.hidden3 = !this.hidden3;
-      }
+      
     },
     computed:{
       
