4f025b03baa1d0296aa2e9432a46a1cd49b3c8e5
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-13 19:50:55 +0300

    Задание58

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,18 @@
 <template>
-	<ul>
-		<li v-for="user in users" :key="user.id">
-			<template v-if="!user.isEdit">
-				{{ user.name }}
-				{{ user.surn }}
-				<button @click="edit(user)">
-					edit
-				</button>
-			</template>
-			<template v-else>
-				<input v-model="user.name">
-				<input v-model="user.surn">
-				<button @click="save(user)">
-					save
-				</button>
-			</template>
-		</li>
-	</ul>
+	<User />
 </template>
 
 <script>
+  import User from './components/User.vue'
   export default{
+    components:{
+      User
+    },
     data(){
       return{
-        users: [
-			{
-				id: 1,
-				name: 'name1',
-				surn: 'surn1',
-				isEdit: false,
-			},
-			{
-				id: 2,
-				name: 'name2',
-				surn: 'surn2',
-				isEdit: false,
-			},
-			{
-				id: 3,
-				name: 'name3',
-				surn: 'surn3',
-				isEdit: false,
-			},
-		]
+        
       }
-    },
-    methods: {
-    edit(user) {
-		  user.isEdit = true;
-	  },
-	  save(user) {
-		  user.isEdit = false;
-	},
-	  }
+	}
 }
 </script>
 
---
+++ src/components/User.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ name }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+		return {
+			name: 'john',
+		}
+	}
+}
+</script>
+
+<style>
+    
+</style>
