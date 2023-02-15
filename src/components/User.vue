429d98aa0976e5a6956668c3cf3933d5a347ff6c
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-15 20:47:46 +0300

    Задание70

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,42 +1,27 @@
 <template>
-	<template v-if="!isEdit">
-		{{ name }}
-		{{ surn }}
-		<button @click="edit">
-			edit
-		</button>
-	</template>
-	<template v-else>
-		<input v-model="newName">
-		<input v-model="newSurn">
-		<button @click="save">
-			save
-		</button>
-	</template>
+	<input v-model="newName">
+	<input v-model="newSurn">
+	
+	<button @click="save">
+		save
+	</button>
 </template>
 
 <script>
-export default {
-    props: {
-	id:     Number,
-	name:   String,
-	surn:   String,
-},
-    emits: ['change'],
+export default{
     data() {
         return {
-            isEdit: false,
-            newName: this.name,
-            newSurn: this.surn,
+            newName: '',
+            newSurn: '',
         }
-    }, 
+    },
     methods: {
-	save() {
-		this.isEdit = false;
-		this.$emit('change', this.id, this.newName, this.newSurn);
-	}
-},  
-};
+        save() {
+            this.$emit('add', this.newName, this.newSurn);
+        }
+    },
+
+}
 </script>
 
 
