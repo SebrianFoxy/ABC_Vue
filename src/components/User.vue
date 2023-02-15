3120b980b1eea06bacbe69addfd069c989419e82
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-15 20:16:42 +0300

    Задание66

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,13 +1,15 @@
 <template>
 <div>
     <h2>Дочерний компонент</h2>
-    <button @click="handle">btn</button>
+    <button @click="$emit('show', 'xxx', 'yyy')">
+		click
+	</button>
 </div>
 </template>
 
 <script>
 export default {
-    emits:['show'],
+    emits: ['show'],
     methods: {
 	    handle() {
 		    this.$emit('show', 'xxx', 'yyy');
