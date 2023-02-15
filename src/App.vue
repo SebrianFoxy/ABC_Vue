c1ee37072a44cac151f457c828aeef2a50ebce9c
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-15 19:01:43 +0300

    Задание62

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <template>
-	<User :arg="[1, 2, 3]" />
+  <div>
+    <h2>Работники:</h2>
+    <ul>
+      <li v-for="user in users" 
+        :name="user.name"
+        :surn="user.surn"
+        :key="user.id">
+        <p>Имя: {{ user.name }}</p>
+        <p>Зарплата: {{ user.salary }}</p>
+        <p>Возраст: {{ user.age }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-  import User from './components/User.vue'
-  export default{
-    components:{
-      User
-    },
-    data(){
-      return{
-        
-      }
-	}
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: 'name1',
+          salary: 100,
+          age: 30,
+        },
+        {
+          id: 2,
+          name: 'name2',
+          salary: 200,
+          age: 40,
+        },
+        {
+          id: 3,
+          name: 'name3',
+          salary: 300,
+          age: 50,
+        },
+      ],
+    }
+  },
 }
 </script>
+
 
 <style> 
 p.done{
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,18 +1,17 @@
 <template>
     <div>
-        {{ arg }}
+        
     </div>
 </template>
 
 <script>
 export default {
     props: {
-		name: String,
-		surn: String,
+        
 	},
     data() {
 		return {
-			
+            
 		}
 	}
 }
