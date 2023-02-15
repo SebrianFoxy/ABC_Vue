55f6a6cc44ea73cf3a9a1f99f7b70a72fc942271
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-02-15 19:28:07 +0300

    Задание63

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,15 @@
 <template>
   <div>
-    <h2>Работники:</h2>
-    <ul>
-      <li v-for="user in users" 
-        :name="user.name"
-        :surn="user.surn"
-        :key="user.id">
-        <p>Имя: {{ user.name }}</p>
-        <p>Зарплата: {{ user.salary }}</p>
-        <p>Возраст: {{ user.age }}</p>
-      </li>
-    </ul>
+    
   </div>
 </template>
 
 <script>
 export default {
+  props: ['initialCounter'],
   data() {
     return {
-      users: [
-        {
-          id: 1,
-          name: 'name1',
-          salary: 100,
-          age: 30,
-        },
-        {
-          id: 2,
-          name: 'name2',
-          salary: 200,
-          age: 40,
-        },
-        {
-          id: 3,
-          name: 'name3',
-          salary: 300,
-          age: 50,
-        },
-      ],
+      counter: this.initialCounter
     }
   },
 }
